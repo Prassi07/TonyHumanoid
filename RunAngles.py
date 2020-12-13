12cee29b4ed8e4aada6a01cf0bbba4b2cc36fb60
Prassi07 <25366464+Prassi07@users.noreply.github.com>
2020-12-13 19:08:18 +0530

    Initial Commit
    ->Added All files

---
+++ RunAngles.py
@@ -0,0 +1,75 @@
+# Author : Prajwal, Prasanna
+# Receievs walking sequence angles from 3D LIP Algorithm
+# Converts it to SERVO mapped angles which can be sent to the bot.
+
+import time
+import serial
+def calculate_PWM(angle):
+    pwm= 500.0 + (((2500.0-500.0)/(180-0))*(angle-0))
+    return pwm
+#filename = str(input("Enter the file Name: "))
+#filename = filename + ".txt";
+f=open("lateralWalk.txt")
+#newfilename = str(input("Enter output file Name : "))
+newfilename = "TEMP" + ".txt"
+f2=open(newfilename,'w');
+angles=f.readlines();
+newangles=[0]*10;
+finalangles=[]
+Offsets=[-50, -10, -20, 0, 0, -70, 100, 68, 0, -40]
+#f2.write("Max: 2500 2500 2500 2500 2500 2500 2500 2500 2500 2500 2500 2500 2500 2500 2500 2500 2500 2500\nMin: 500 500 500 500 500 500 500 500 500 500 500 500 500 500 500 500 500 500\nCen: 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500\nHom: 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500 1500\nDir: True True True True True True True True True True True True True True True True True True\nLab:Servo 1:Servo 2:Servo 3:Servo 4:Servo 5:Servo 6:Servo 7:Servo 8:Servo 9:Servo 10:Servo 11:Servo 12:Servo 13:Servo 14:Servo 15:Servo 16:Servo 17:Servo 18:\nPrm: 0 2 1 0 1\nGrp: 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n")   
+print(len(angles))
+for step in angles:
+    current_step=step.split();
+    newangles[0]=90-(int(current_step[0])*1.4) #theta1 1.4
+    newangles[1]=90+int(current_step[1])  #theta2
+    newangles[2]=180-(30+abs(int(current_step[2]))) #theta3
+    newangles[3]=90+int(current_step[3]) #theta4
+    newangles[4]=90+(1.0*int(current_step[4])) #theta5 1.4
+    newangles[5]=90-(1.0*int(current_step[5]))#theta6 1.4
+    newangles[6]=180-(90+int(current_step[6])) #theta7
+    newangles[7]=30+abs(int(current_step[7])) #theta8
+    newangles[8]=180-(90+int(current_step[8])) #theta9
+    newangles[9]=90+(int(current_step[9])*1.4) #theta10 
+    #print(current_step[0],current_step[1],current_step[2],current_step[3],current_step[4],current_step[5],current_step[6],current_step[7],current_step[8],current_step[9])
+    print(newangles)
+    current_pwm=[]
+    f2.write("L=")
+    offNum = 0
+    for i in newangles:
+        pwm=calculate_PWM(i)+Offsets[offNum];
+        current_pwm.append(int(pwm))
+        offNum = offNum+1
+        f2.write(str(int(pwm))+":")
+    f2.write('\n');
+f2.close()
+f.close()
+#######################
+ser = serial.Serial("COM4",115200);
+toWait = 1
+#speed = int(input("Speed of the Servos?? : "))
+num=3
+line = ser.readline()
+print(line)
+speedString = "P=80\n"
+print(speedString)
+ser.write(speedString.encode('utf-8'))
+while(num>=0):
+    f2 = open(newfilename)
+    toBeSent = f2.readlines()
+    #toBeSent.reverse()
+    #c = ser.read()
+    for line in toBeSent:
+        #toWait=1
+        ser.write(line.encode('utf-8'))
+        while(1):
+            c = ser.read();
+            print(c)
+            if c == b'i':
+                break;
+        while(toWait != 0):
+            key = input("Press any number to Continue Sending :" )
+            toWait = 0
+    num=num-1
+    f2.close()
+ser.close
---
+++ TonyIK.m
@@ -0,0 +1,97 @@
+%% Function to Solve inverse kinematics of 5-dof leg of humanoind robot
+%% Inputs = 6 co-ordinates, from x,y,z foot co-ordinates and x,y,z hip
+%%% co-ordinates/
+%% Returns 5 angles - hip roll, pitch. knee pitch. ankle pitch, roll
+%%% Based on Equations in the Paper:
+%%% Solving Inverse Kinematics using Geometric Anaylsis on Small-Scale
+%%% Humanoid Robot submitted to IEEE/SICE International Symposium on System
+%%% Integration 2020
+%% Authors: Prasanna Venkatesan K S and Prajwal Rajendra Mahendrakar
+
+function [te1,te2,te3,te4,te5] = TonyIK(xf,yf,zf,x1,y1,z1,leg)
+%Robot Parameters[45,65,65,50,17]
+l1 =1;
+l2 =93;
+l3 =93;
+l4 =1;
+l5 =33.5;
+xf = round(xf,2);
+yf = round(yf,2);
+zf = round(zf,2);
+x1 = round(x1,2);
+y1 = round(y1,2);
+z1 = round(z1,2);
+%Intialize known co-ordinates
+options = optimset('Display','off');
+y2 = y1;
+y5 = yf;
+y4 = y5;
+x5 = xf;
+z5 = zf + l5;
+%calculate slope of the leg
+slope = (z1 - z5)/(x1 - x5);
+%solve equations
+ if (slope == Inf) || (x1 == x5)
+    %Solve for x2 and z2
+    x2 = x1;
+    z2 = z1 - l1;
+    te1 = atan((x1-x2)/(z1-z2));
+    te5 = -te1;
+    %solve for x4 and z4
+    x4 = x5;
+    z4 = z5 + l4;
+    %solve for x3, y3, z3
+    x3 = x2;
+    %Solve for y3 and z3
+
+     myfun = @(x)[(x(1)-y4)^2 + (x(2)-z4)^2 - l3^2 ,
+                (y2-x(1))^2 + (z2-x(2))^2 - l2^2];
+     sol = fsolve(myfun,[z2,0],options);
+     y3 = sol(1);
+     z3 = sol(2);
+ else
+    %Solve for x2 and z2
+    myfun1 = @(x) [(x1 - x(1))^2 + (z1 - x(2))^2 - l1^2 ,
+                    z1 - x(2) + slope*x(1) - slope*x1 ];
+    sol = fsolve(myfun1,[x5,z1],options);
+    x2 = sol(1);
+    z2 = sol(2);
+    
+    myfun2 = @(x)[ ( x(1) - x5)^2 + (x(2) - z5)^2 - l4^2 ,
+                     x(2) - z5  - slope *x(1) + slope*x5 ];
+    sol = fsolve(myfun2,[0 ,z2],options);
+    x4 = sol(1);
+    z4 = sol(2);
+    
+    te1 = - atan((x1-x2)/(z1-z2));
+    te5 = -te1;
+    
+    myfun3 = @(x)[ (x(1) - x4)^2 + (x(2) - y4)^2 + (x(3) - z4)^2 - l3^2 , 
+                   (x2 - x(1))^2 + (y2 - x(2))^2 + (z2 - x(3))^2 - l2^2 ,
+                    x(3) - z4 - slope*x(1) + slope* x4 ];
+
+    sol = fsolve(myfun3,[0,z1,0],options);
+    x3 = sol(1);
+    y3 = sol(2);
+    z3 = sol(3);
+   
+ end
+ if(leg == 'r')
+    fprintf(" xr1 = %f; yr1 = %f; zr1 = %f;  xr2 = %f; yr2 = %f; zr2 = %f;\n", x1,y1,z1,x2,y2,z2);
+    fprintf(" xr3 = %f; yr3 = %f; zr3 = %f;  xr4 = %f; yr4 = %f; zr4 = %f;\n", x3,y3,z3,x4,y4,z4);
+    fprintf(" xr5 = %f; yr5 = %f; zr5 = %f;  xrf = %f; yrf = %f; zrf = %f;\n", x5,y5,z5,xf,yf,zf);
+ else
+    fprintf(" xl1 = %f; yl1 = %f; zl1 = %f;  xl2 = %f; yl2 = %f; zl2 = %f;\n", x1,y1,z1,x2,y2,z2);
+    fprintf(" xl3 = %f; yl3 = %f; zl3 = %f;  xl4 = %f; yl4 = %f; zl4 = %f;\n", x3,y3,z3,x4,y4,z4);
+    fprintf(" xl5 = %f; yl5 = %f; zl5 = %f;  xlf = %f; ylf = %f; zlf = %f;\n", x5,y5,z5,xf,yf,zf);
+ end
+    te2 = abs(atan((y3-y2)/(z2-z3)));
+    te4 = abs(atan2((y3-y4),(z3-z4)));
+    te3 = -(abs(te2)+abs(te4));
+
+    te1 = te1*180/pi;
+    te2 = te2*180/pi;
+    te3 = te3*180/pi;
+    te4 = te4*180/pi;
+    te5 = te5*180/pi;
+end
